c28cd23fda9e9772d3eb6cc0e8024a856a1ed8b4
Jan Nakladal <mojeto1@gmail.com>
2016-07-18 15:29:34 +1200

    version 0.4.3

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
